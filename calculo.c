5d6bfa5403f2988f55ae692f765055000c35e653
Edinei Lucas Silva Souza <edineilucas001@gmail.com>
2025-10-02 08:20:20 -0300

    Atividade prevista em lab03.h

---
+++ calculo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+le inteiro, depois operação + inteiro
+le com %s
+*/
---
+++ lab03.h
@@ -0,0 +1,7 @@
+Escreva um programa de computador que leia uma única linha contendo 
+uma expressão aritmética de número naturais contendo as operações
+de soma, subtração, multiplicação e divisão (divisão inteira). A expressão
+está no formato em que entre cada número e expressão há um espaço em
+branco. Por exemplo: "10 + 20 + 30 * 40 - 50".
+
+Seu programa deve imprimir uma única linha contendo o resultado da expressão.
